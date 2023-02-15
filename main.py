667114d78b8065a699fe1d0f459481cbb6661cd5
niols <niolsson@gmail.com>
2023-02-15 21:35:56 +0100

    Added pause functionality

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ screen = pygame.display.set_mode((500, 600), pygame.RESIZABLE)
 
 clock = pygame.time.Clock()
 
+
+pygame.display.set_caption("Tetris")
+
 gameRate = 10
 rateCounter = 0
 
@@ -17,25 +20,36 @@ d, l, r = 0, 0, 0
 graphics = Graphics(20, 10, screen)
 model = Model()
 
+graphics.repaint(model.get_board(), model.get_score())
+pygame.display.flip()
+
+paused = True
 running = True
 while running:
     clock.tick(60)
-    pygame.display.set_caption("Tetris")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            model.key_pressed(event.key)
+            if paused:
+                if event.key == pygame.K_SPACE:
+                    paused = False
+            else:
+                if event.key == pygame.K_ESCAPE:
+                    paused = True
+                else:
+                    model.key_pressed(event.key)
         elif event.type == pygame.VIDEORESIZE:
             screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
 
-    if rateCounter == gameRate:
-        rateCounter = 0
-        model.tick()
-    else:
-        rateCounter += 1
+    if not paused:
+        if rateCounter == gameRate:
+            rateCounter = 0
+            model.tick()
+        else:
+            rateCounter += 1
 
-    graphics.repaint(model.get_board(), model.get_score())
-    pygame.display.flip()
+        graphics.repaint(model.get_board(), model.get_score())
+        pygame.display.flip()
 
 pygame.quit()
